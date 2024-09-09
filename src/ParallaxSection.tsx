d4a3d703ec94fd43942f9cc4ad5da440cd6f1b5f
deepakvjnbb <deepakvjn.bb@gmail.com>
2024-09-09 23:30:54 +0530

    version 2h

--- src/ParallaxSection.tsx
+++ src/ParallaxSection.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Rellax from 'rellax';
 
 interface ParallaxSectionProps {
   backgroundImage: string;
-  children: React.ReactNode;
+  children: React.ReactNode; // ReactNode to allow any valid React child elements
 }
 
 const ParallaxSection: React.FC<ParallaxSectionProps> = ({ backgroundImage, children }) => {
+  useEffect(() => {
+    // Initialize Rellax for parallax effect
+    new Rellax('.rellax', {
+      speed: -2,
+      center: true, // Centers the parallax effect
+      round: true,
+    });
+  }, []);
+
   return (
     <section
       style={{
         backgroundImage: `url(${backgroundImage})`,
-        backgroundAttachment: 'fixed',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
-      className="relative h-screen flex flex-col justify-center items-center text-center"
+      className="rellax relative h-screen flex flex-col justify-center items-center text-center"
     >
       {children}
     </section>
