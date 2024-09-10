328d4902ddca7ff5e15bb7645b7409a8108714bf
deepakvjnbb <deepakvjn.bb@gmail.com>
2024-09-10 11:42:52 +0530

    version 3o

--- src/ParallaxSection.tsx
+++ src/ParallaxSection.tsx
@@ -20,38 +20,27 @@ const ParallaxSection: React.FC<ParallaxSectionProps> = ({ imageClass, children 
 
   return (
     <Parallax
-  bgImage={getImage(imageClass)}
-  bgImageAlt="the background"
-  strength={300}
-  bgImageStyle={{
-    objectFit: 'cover',
-    width: '100%', // Ensure width and height are set
-    height: '100%',
-    transform: 'translate3d(0,0,0)' // Add this for smoother rendering
-  }}
->
-    {/* // <Parallax
-    //   bgImage={getImage(imageClass)} // Dynamically set the image
-    //   bgImageAlt="the background"
-    //   strength={300}
-    //   bgImageStyle={{
-    //     objectFit: 'cover',
-    //     width: '100%',
-    //     height: '100%',
-    //   }}
-    //   style={{
-    //     height: '100vh',
-    //     width: '100%',
-    //     margin: 0,
-    //     padding: 0,
-    //     willChange: 'transform',
-    //   }}
-    //   className={`parallax-container ${imageClass}`}
-    // > */}
-    //   <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-x-hidden">
-    //     {children}
-    //   </div>
-    // </Parallax>
+      bgImage={getImage(imageClass)} // Dynamically set the image
+      bgImageAlt="the background"
+      strength={200}
+      bgImageStyle={{
+        objectFit: 'cover',
+        width: '100%',
+        height: '100%',
+      }}
+      style={{
+        height: '100vh',
+        width: '100%',
+        margin: 0,
+        padding: 0,
+        willChange: 'transform',
+      }}
+      className={`parallax-container ${imageClass}`}
+    >
+      <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-x-hidden">
+        {children}
+      </div>
+    </Parallax>
   );
 };
 
